b16a3f233703d61420269203e90936a22220dfdb
Esha-Shahid <websitedeveloper09@gmail.com>
2024-11-22 15:36:11 +0500

    new task

---
+++ practice-task-4.js
@@ -0,0 +1,11 @@
+let Dal = 150;
+let Naan = 40;
+let ice_cream = 50;
+
+document.write("Dal = ", Dal);
+document.write("Naan = \n",Naan);
+document.write("Ice-Cream = \n",ice_cream);
+
+let Finall_bill = Dal + Naan + ice_cream;
+
+document.write("According to your order, your bill..:", Finall_bill);
